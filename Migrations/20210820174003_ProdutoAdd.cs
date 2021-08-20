7b128f63a4b8d01cf6b4b732bf1a3f81519a85f7
Wesley Americo <2048507@TCS.com>
2021-08-20 14:40:41 -0300

    Adicionando migrations de produtos

---
+++ Migrations/20210820174003_ProdutoAdd.cs
@@ -0,0 +1,39 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace EntityFrameWork.Migrations
+{
+    public partial class ProdutoAdd : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropPrimaryKey(
+                name: "PK_Clientes",
+                table: "Clientes");
+
+            migrationBuilder.RenameTable(
+                name: "Clientes",
+                newName: "clientes");
+
+            migrationBuilder.AddPrimaryKey(
+                name: "PK_clientes",
+                table: "clientes",
+                column: "id");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropPrimaryKey(
+                name: "PK_clientes",
+                table: "clientes");
+
+            migrationBuilder.RenameTable(
+                name: "clientes",
+                newName: "Clientes");
+
+            migrationBuilder.AddPrimaryKey(
+                name: "PK_Clientes",
+                table: "Clientes",
+                column: "id");
+        }
+    }
+}
---
+++ Models/Produto.cs
@@ -0,0 +1,37 @@
+using System;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace EntityFrameWork.Models
+{
+    [Table("produtos")]
+    public class Produto
+    {
+        [Key]
+        [Required]
+        [Column("id")]
+        public int Id { get; set; }
+
+        [MaxLength(150)]
+        [Required]
+        [Column("nome")]
+        public string Nome {get; set;}
+
+        [MaxLength(255)]
+        [Column("imagem")]
+        public string UrlImagem {get; set;}
+
+
+        [Required]
+        [Column("valor")]
+        public double Valor {get; set;}
+
+        
+      
+        [Column("descricao",TypeName ="text")]
+        public string Descricao {get; set;}
+
+       
+        
+    }
+}
