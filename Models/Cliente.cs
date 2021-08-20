b4bbdbafdf450a4283715a57e6a5a5c0c96ca17a
Wesley Americo <2048507@TCS.com>
2021-08-20 11:06:55 -0300

    criando o modelo cliente

---
+++ Models/Cliente.cs
@@ -0,0 +1,29 @@
+using System;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace EntityFrameWork.Models
+{
+    public class Cliente
+    {
+        [Key]
+        [Required]
+        [Column("id")]
+        public int Id { get; set; }
+
+        [MaxLength(150)]
+        [Required]
+        [Column("nome")]
+        public string Nome {get; set;}
+
+        [MaxLength(255)]
+        [Required]
+        [Column("endereco")]
+        public string Endereco {get; set;}
+
+      
+        [Required]
+        [Column("observacao",TypeName ="text")]
+        public string Observacao {get; set;}
+    }
+}
--- Servicos/DbContexto.cs
+++ Servicos/DbContexto.cs
@@ -1,10 +1,12 @@
 using System;
+using EntityFrameWork.Models;
 using Microsoft.EntityFrameworkCore;
 
 namespace EntityFrameWork.Servicos
 {
     public class DbContexto : DbContext
     {
-
+        public DbContexto(DbContextOptions<DbContexto> options) : base(options){}
+        public DbSet<Cliente> Clientes {get; set;}
     }
 }
